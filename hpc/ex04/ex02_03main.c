0883c144002f70a6d15c9020949e9c7eff5f1841
nilhahn <matzeheisler@googlemail.com>
2014-11-17 23:04:43 +0100

    new version

--- hpc/ex04/ex02_03main.c
+++ hpc/ex04/ex02_03main.c
@@ -6,7 +6,7 @@
 #define TRUE 1
 #define FALSE 0
 
-#define DEBUG FALSE
+#define DEBUG TRUE
 
 #define SIZE 2048
 
@@ -23,8 +23,11 @@ void initMatrix(double* matr,int dim,int multiply);
 void doBoss(int size, int matrSize, int initStyle);
 void doWorker(int rank,int size, int matrSize);
 
+double* reorderMatrix(double *matr,unsigned size);
+
 void plot(double* mat,int dim);
 
+/**MAIN**/
 int main(int argc,char** argv){
 
 	int rank,size;
@@ -48,6 +51,8 @@ int main(int argc,char** argv){
 			initStyle = INIT_STYLE_RANDOM;
 		}
 	}
+
+	printf("%d matrsize: %d\n",rank,matrSize);
 
 	if(rank == 0){
 		//boss node;
@@ -117,15 +122,24 @@ void initMatrix(double* matr,int dim,int multiply){
 void doBoss(int size, int matrSize, int initStyle){
 	double* matr1 = NULL;
 	double* matr2 = NULL;
+	double* bufferMatr = NULL;
 	double* recvBuffer = NULL;
 	double* matrErg = NULL;
 	
 	int i = 1;
 	int j = 0;
+	int h = 0;
+
 	int workerSize = size -1;
 	int receiveBufferSize = 0;
 	int recvElem = 0;
-	
+
+	int rows = matrSize;
+	int workerRows = 0;
+	int workerRowDiff = 0;
+	int offset = 0;
+	int bufferMatrSize = 0;
+
 	struct timeval start,end;
 	long runtime; //runtime in us;
 	float runtimeSec; //runtime in sec;
@@ -133,13 +147,12 @@ void doBoss(int size, int matrSize, int initStyle){
 
 	MPI_Status status; 	//recv status;
 	int recvSize;		//number of received entries;
-	
-	
+
 	matr1 = (double*)malloc(matrSize*matrSize*sizeof(double));
 	matr2 = (double*)malloc(matrSize*matrSize*sizeof(double));
-	
+
 	receiveBufferSize = matrSize;
-	
+
 	if(workerSize & 0x01 && workerSize > 1){ //odd amount of worker
 		receiveBufferSize*=((matrSize/workerSize)+1);
 	}else{
@@ -158,6 +171,8 @@ void doBoss(int size, int matrSize, int initStyle){
 		initMatrix(matr2,matrSize,FALSE);	 // matr2[i,j] = i * j;
 	}
 
+	matr2 = reorderMatrix(matr2,matrSize);
+
 	initMatrixRandom(matrErg,matrSize,FALSE); //matrErg = 0.0;
 
 	#if DEBUG == TRUE
@@ -175,6 +190,44 @@ void doBoss(int size, int matrSize, int initStyle){
 		MPI_Send(matr2, matrSize*matrSize, MPI_DOUBLE, i, 2, MPI_COMM_WORLD);
 	} 
 
+	//check if there something left to do for the boss
+	if(matrSize % workerSize > 0){
+		for(i = 1; i <= workerSize; i++){
+			if(workerSize & 0x01 && workerSize > 1){ //odd amount of worker
+				workerRows=((matrSize/workerSize)+1);
+			}else{
+				workerRows=(matrSize/workerSize);
+			}
+
+			workerRowDiff = matrSize - i * workerRows;
+
+			if(workerRowDiff < 0){
+				workerRows = workerRows + workerRowDiff;
+			}
+			
+			//initial Value for rows = matrSize
+			//if rows > 0 => there are work to do
+			rows = rows - workerRows;
+		}
+		
+		#if DEBUG == TRUE
+		printf(" Boss should calculate %d rows \n",rows);
+		#endif
+
+		if(rows > 0){
+			offset = (matrSize - rows)*matrSize;
+			bufferMatrSize = matrSize*rows;
+			bufferMatr = (double*)malloc(bufferMatrSize*sizeof(double));
+			for(i = 0;i < rows; i++){//rows of the result-matrix
+				for(j = 0; j < matrSize; j++){//colums of the result-matrix
+					for(h = 0; h < matrSize; h++){
+						*(bufferMatr+j+i*matrSize)+=(*(matr1+h+offset+i*matrSize))*(*(matr2+j*matrSize+h));
+					}
+				}
+			}
+		}
+	}
+
 	//gather Results from worker-nodes
 	for(i = 1; i < size; i++){
 		MPI_Recv(recvBuffer,receiveBufferSize,MPI_DOUBLE,i,0,MPI_COMM_WORLD,&status);
@@ -190,12 +243,18 @@ void doBoss(int size, int matrSize, int initStyle){
 
 		recvElem += recvSize;
 	}
-	
+
+	if(rows > 0){
+		for(i = 0; i < bufferMatrSize;i++){
+			*(matrErg+recvElem+i)= *(bufferMatr+i);
+		}
+	}	
+
 	gettimeofday(&end,NULL);
 	
 	#if DEBUG == TRUE
 		printf("Result\n");
-		plot(matrErg,SIZE);
+		plot(matrErg,matrSize);
 	#endif
 
 	runtime = calcRuntime(&start,&end);
@@ -203,13 +262,42 @@ void doBoss(int size, int matrSize, int initStyle){
 	flops = (2*size*size)/(runtimeSec); //??
 
 	printf("#runtime[us] runtime[sec] flop size\n");
-	printf("%ld %f %f %d\n",runtime,runtimeSec,flops,SIZE);
+	printf("%ld %f %f %d\n",runtime,runtimeSec,flops,matrSize);
 	
-	free(matr1);
-	free(matr2);
-	free(matrErg);
-	free(recvBuffer);
+	if(matr1 != NULL){	
+		#if DEBUG == TRUE
+		printf("Release memory for matr1\n");
+		#endif	
+		free(matr1);
+	}
+
+	if(matr2 != NULL){
+		#if DEBUG == TRUE
+		printf("Release memory for matr2\n");
+		#endif
+		free(matr2);
+	}
 	
+	if(matrErg != NULL){
+		#if DEBUG == TRUE
+		printf("Release memory for matrErg\n");
+		#endif
+		free(matrErg);
+	}
+	
+	if(recvBuffer != NULL){
+		#if DEBUG == TRUE
+		printf("Release memory for recvBuffer\n");
+		#endif 
+		free(recvBuffer);
+	}
+
+	if(bufferMatr != NULL){
+		#if DEBUG == TRUE
+			printf("Release memory for bufferMatr\n");
+		#endif
+		free(bufferMatr);
+	}
 }
 
 void doWorker(int rank,int size, int matrSize){
@@ -227,22 +315,26 @@ void doWorker(int rank,int size, int matrSize){
 	
 	MPI_Status status;
 	
-	matr1 = (double*)malloc(SIZE*SIZE*sizeof(double));
-	matr2 = (double*)malloc(SIZE*SIZE*sizeof(double));
+	matr1 = (double*)malloc(matrSize*matrSize*sizeof(double));
+	matr2 = (double*)malloc(matrSize*matrSize*sizeof(double));
 
 	if(worker & 0x01 && worker > 1){ //odd amount of worker
-		rows=((SIZE/worker)+1);
+		rows=((matrSize/worker)+1);
 	}else{
-		rows=(SIZE/worker);
-	}
-	
-	rowdiff = maxRows - rank*rows;
-	if(rowdiff < 0){
-		rows = rows - rowdiff;
+		rows=(matrSize/worker);
 	}
 
 	//TODO: Implement Error handling for the case workSize <= 0;
 	workSize*=rows;
+
+	offset = (rank -1)*workSize; //workSize is here used as an Helpervalue
+
+	rowdiff = maxRows - rank*rows;
+	if(rowdiff < 0){
+		rows = rows + rowdiff;
+	}
+	
+	workSize = matrSize*rows;	
 	erg = (double*)malloc(workSize*sizeof(double));
 	
 	MPI_Recv(matr1,SIZE*SIZE,MPI_DOUBLE,0,1,MPI_COMM_WORLD,&status);
@@ -255,27 +347,52 @@ void doWorker(int rank,int size, int matrSize){
 	#if DEBUG == TRUE
 		printf("%d has received matrix no. 2\n",rank);
 	#endif
-	
-	//do work (calculate)
-	offset = (rank-1)*workSize;
+
 	#if DEBUG == TRUE
-		printf("%d offset %d\n",rank,offset);
+		printf("%d worksize %d rows %d offset %d\n",rank,workSize,rows,offset);
 	#endif	
 
 	for(i = 0;i < rows; i++){//rows of the result-matrix
 		for(j = 0; j < matrSize; j++){//colums of the result-matrix
 			for(h = 0; h < matrSize; h++){
-				*(erg+j+i*matrSize)+=(*(matr1+h+offset+i*matrSize))*(*(matr2+j+h*matrSize));
+				*(erg+j+i*matrSize)+=(*(matr1+h+offset+i*matrSize))*(*(matr2+j*matrSize+h));
 			}
 		}
 	}
 
 	//Send Result back to boss node
 	MPI_Send(erg,workSize,MPI_DOUBLE,0,0,MPI_COMM_WORLD);
+	
+	if(matr1 != NULL){
+		free(matr1);
+	}
+	
+	if(matr2 != NULL){
+		free(matr2);
+	}
+	
+	if(erg != NULL){
+		free(erg);
+	}
+}
 
-	free(matr1);
-	free(matr2);
-	free(erg);
+/*Function: reorderMatrix() */
+double* reorderMatrix(double *matr,unsigned size){
+	unsigned i = 0;
+	unsigned j = 0;
+	unsigned matrSize = size*size;
+	double* exchangeMatr = NULL;
+
+	exchangeMatr = (double*)malloc(matrSize*sizeof(double));
+
+	for (i = 0; i < size; i++){
+		for (j = 0; j < size; j++){
+			*(exchangeMatr + i*size + j) = *(matr + i + j*size);
+		}
+	}
+	
+	free(matr);
+	return exchangeMatr;
 }
 
 /*Function: plot()*/
